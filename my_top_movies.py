989d277e4709163afd4ce3abdc5f1f5dddfbf5d9
Iwona <iwona.skok@gmail.com>
2024-09-16 21:11:50 +0200

    filmy progres

---
+++ my_top_movies.py
@@ -0,0 +1,36 @@
+favourite_movies = [
+    {
+        "title": "Purple hearts",
+        "year": 2022,
+        "rating": 6.7,
+        "description": "W tym romantycznym dramacie szukająca sławy piosenkarka Cassie (Sofia Carson) i były komandos z trudną przeszłością Luke (Nicholas Galitzine) decydują się na małżeństwo z rozsądku.",
+    },
+    {
+        "title": "Requiem for a dream",
+        "year": 2000,
+        "rating": 7.8,
+        "description": "Wstrząsająca opowieść o czwórce bohaterów z Brooklynu, którzy w pogoni za realizacją swoich marzeń odbywają podróż w głąb piekła uzależnień",
+    },
+    {
+        "title": "Kamerdyner",
+        "year": 2018,
+        "rating": 6.5,
+        "description": "Kaszubski chłopiec, po śmierci matki zostaje przygarnięty przez arystokratkę Gerdę von Krauss pochodzącą z majętnego pruskiego rodu. Dorasta w pałacu. Rówieśniczką Mateusza jest córka Kraussów Marita. Między młodymi rodzi się miłość. ",
+    },
+    {
+        "title": "The wolf of Wall Street",
+        "year": 2013,
+        "rating": 7.7,
+        "description": "Jordan Belfort był złotym dzieckiem świata amerykańskich finansów. Szybki i oszałamiający sukces przyniósł mu fortunę, władzę i poczucie bezkarności. Pokusy czaiły się wszędzie, a Belfort lubił im ulegać i robił to w wielkim stylu.",
+    },
+]
+print("Tytuł pierwszego filmu to:")
+print(favourite_movies[0]["title"])
+print("Rok premiery drugiego filmu to:")
+print(favourite_movies[1]["year"])
+print("Ocena IMDB trzeciego filmu to:")
+print(favourite_movies[2]["rating"])
+print("Krótki opis czwartego filmu to:")
+print(favourite_movies[3]["description"])
+
+    
